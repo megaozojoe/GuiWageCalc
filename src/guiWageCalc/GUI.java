cbc6153c32dbb219435e7b2228a1169f9b97bce2
Matthew <matthewadelreal@gmail.com>
2017-05-25 15:08:43 -0400

    Readded the seperate method. Added the method for the second by second total. Does not work

--- src/guiWageCalc/GUI.java
+++ src/guiWageCalc/GUI.java
@@ -8,13 +8,14 @@ public class GUI extends JFrame{
 	
 	
 	static final JTextField wagebx = new JTextField();	
-	static boolean pressed = true;
+	static boolean runBL = false;
 	static JButton calcBN = new JButton("Clock in!");
 	static double wage = 0.0;
 	static JLabel moneyLB = new JLabel("0.0");
-	static int inc = 0;
 	static String temp = null;
 	static double total = 0.0;
+	static double wagesec = 0.0;
+	
 	
 	static JFrame fme = new JFrame("Wage Calculator");
 	
@@ -34,19 +35,42 @@ public class GUI extends JFrame{
 		calcBN.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				
-				wage = Integer.parseInt(wagebx.getText());
-				
-				
-				total = total + wage;
-				
-				
-				temp = Double.toString(total);
-				moneyLB.setText(temp);	
-				
+				calcing();
 			}
 		});
+		while(runBL){
+			wagesec = wagesec + wage;
+			temp = Double.toString(wagesec);
+			moneyLB.setText(temp);	
+			
+		}
+		
+		
 		
 		frame();
+		
+	}
+	
+	public static void calcing(){
+		
+		wage = Integer.parseInt(wagebx.getText());
+		
+		wage = wage/60;
+		wage = wage/60;
+		
+		runBL = !runBL;
+		
+		if(runBL){
+			
+			calcBN.setText("Clock out!");
+			
+						
+		}
+		else if(runBL){
+			calcBN.setText("Clock out!");
+		}
+		
+		
 		
 	}
 	
